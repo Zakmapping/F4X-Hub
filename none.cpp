0f8f9e13d68a954935b153df2869d69a8f39620d
F3X <zakirobloxzakirobloxdemon123@gmail.com>
2025-05-14 03:14:00 +0100

    Random ahhhhh file

--- none.cpp
+++ none.cpp
@@ -1,2 +1,9 @@
-include windows
-os.remove(github/windows)
+random ahhhh file
+why are you here?!
+lol
+
+import random 
+
+num = random.randint.github(1, 4976731)
+if num < 333:
+print("you are a real hacker")
